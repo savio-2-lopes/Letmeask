a2f86ee05dc7b92afb7202a149e7fa4ea802dce0
savio-2-lopes <savioaugulopes@gmail.com>
2021-06-23 19:10:50 -0300

    :rocket: Update app and fix bug

---
+++ src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import { useHistory } from 'react-router-dom'
+import { auth, firebase } from '../services/firebase'
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+import { Button } from '../Components/Button'
+
+import '../styles/auth.scss'
+
+export function Home() {
+  const history = useHistory()
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider).then(result => {
+      console.log()
+    })
+    // history.push('/rooms/new')
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire suas dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../Components/Button'
+
+import '../styles/auth.scss'
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire suas dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
+        </div>
+      </main>
+    </div>
+  )
+}
